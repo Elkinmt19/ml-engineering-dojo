a6f501c869237fc89595bba781ac93dd11608c5d
Elkinmt19 <elkinmt19@gmail.com>
2022-02-25 18:19:53 -0500

    Made de initial setup of the trening-youtube-videos-analysis project

---
+++ sklearn-python/trending-youtube-videos-analysis/get_path_dataset_folder.py
@@ -0,0 +1,18 @@
+# Built-int imports
+import os
+
+
+def get_dataset_folder_path():
+    # Dataset folder which is in the same level of the file
+    WORK_FOLDER = os.path.abspath(
+        os.path.dirname(__file__)
+    )
+
+    DATASET_FOLDER_PATH = os.path.abspath(
+        os.path.join(WORK_FOLDER, "assets")
+    )
+    return DATASET_FOLDER_PATH
+
+
+if __name__ == "__main__":
+    print(get_dataset_folder_path())
