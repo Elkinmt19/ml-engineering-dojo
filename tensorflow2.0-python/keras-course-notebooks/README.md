39c3c26f86f3d6e87a4f069e7030b1c80d5bd130
Elkin Javier Guerra Galeano <elkin.guerra@pragma.com.co>
2022-03-15 08:21:28 -0500

    Created the main structure of the <time-series-forecasting> module and the added the firts file of the <keras-course-notebooks> module

---
+++ tensorflow2.0-python/keras-course-notebooks/README.md
@@ -0,0 +1,3 @@
+# Keras with TensorFlow Course - Python Deep Learning and Neural Networks
+This course is from freeCodeCamp.org. </br>
+[Keras with TensorFlow Course - Python Deep Learning and Neural Networks for Beginners Tutorial](https://www.youtube.com/watch?v=qFJeN9V1ZsI&list=PLrNMDRgYTobhrPDVRkbRY6WbNi9WwlcLy&index=4&t=2672s)
---
+++ tensorflow2.0-python/time-series-forecasting/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+
+# Get README.md long description to add it to the builded package
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+
+    # Library name (same as "pip install ____")
+    name='classchallenges',
+
+    author='Elkin Javier Guerra Galeano',
+
+    # Package version (MAJOR_VERSION.MINOR_VERSION.MAINTENANCE_VERSION)
+    version='0.0.1',
+
+    url="https://github.com/Elkinmt19/data-science-dojo",
+
+    # Simple package description
+    description= "This is a time series module implemented using Tensorflow and Pandas.",
+
+    # Long package description
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    # Our main folder to build the package
+    package_dir={'': 'src'},
+
+    # Add library dependencies
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "pandas",
+        "tensorflow"
+    ],
+
+    # Add devlopment dependencies
+    extras_require={
+        "dev": [
+            "pytest",
+        ],
+    },
+
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha"
+    ],
+
+    license="MIT License",
+
+    # Add extra XML and JSONs needed
+    include_package_data=True
+)
