dc709df54a99bf5688631a9e22f2782da07e9156
Elkin Javier Guerra Galeano <elkinmt19@gmail.com>
2022-03-24 17:10:51 -0500

    Added the <parallelized-data-distribution> AWS SageMaker workshop to the repository

---
+++ aws-sagemaker/sagemaker-workshops/parallelized-data-distribution/replicated_training_job.sh
@@ -0,0 +1,26 @@
+# Create a AWS SageMaker training job using the AWS CLI
+
+# Fill in the values of these four variables
+arn_role=<arn-of-your-notebook-role>
+training_image=<training-image-for-region>
+bucket=<name-of-your-s3-bucket>
+region=<your-region>
+
+prefix=/sagemaker/data_distribution_types
+training_job_name=linear-replicated-`date '+%Y-%m-%d-%H-%M-%S'`
+
+training_data=$bucket$prefix/train
+eval_data=$bucket$prefix/validation
+train_source={S3DataSource={S3DataType=S3Prefix,S3DataDistributionType=FullyReplicated,S3Uri=$training_data}}
+eval_source={S3DataSource={S3DataType=S3Prefix,S3DataDistributionType=FullyReplicated,S3Uri=$eval_data}}
+
+aws --region $region \
+sagemaker create-training-job \
+--role-arn $arn_role \
+--training-job-name $training_job_name \
+--algorithm-specification TrainingImage=$training_image,TrainingInputMode=File \
+--resource-config InstanceCount=5,InstanceType=ml.c4.2xlarge,VolumeSizeInGB=10 \
+--input-data-config ChannelName=train,DataSource=$train_source,CompressionType=None,RecordWrapperType=None ChannelName=validation,DataSource=$eval_source,CompressionType=None,RecordWrapperType=None \
+--output-data-config S3OutputPath=$bucket$prefix \
+--hyper-parameters feature_dim=25,mini_batch_size=500,predictor_type=regressor,epochs=2,num_models=32,loss=absolute_loss \
+--stopping-condition MaxRuntimeInSeconds=1800
