d6480f713ee22489fa055dd61e1567348638f7f7
Elkin Javier Guerra Galeano <elkinmt19@gmail.com>
2022-05-30 19:54:55 -0500

    Added the <setup.py> file which is the setup project file for the second module of the course

---
+++ mlops/mlops-zoomcamp/02_experiment_tracking/setup.py
@@ -0,0 +1,10 @@
+# Build-in imports
+from distutils.core import setup
+
+setup(
+    name="mlops_zoomcamp_02_experiment_tracking",
+    version="0.0.1",
+    description="""
+        This is a project that contains the scripts and notebooks of the module 02 of the MLOps ZoomCamp course.
+    """
+)
