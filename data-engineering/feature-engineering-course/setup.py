6b86e61bec79d8282fe303c1cf02fcb77b76aaa8
Elkin Javier Guerra Galeano <elkinmt19@gmail.com>
2022-06-27 14:12:06 -0500

    Added the <setup.py> of the feature-engineering-course project in order to package the code

---
+++ data-engineering/feature-engineering-course/setup.py
@@ -0,0 +1,11 @@
+# Build-in imports
+from setuptools import setup, find_packages
+
+setup(
+    name="feature_engineering_course",
+    version="0.0.1",
+    description="""
+        This is a project that contains notebooks and exercises of feature engineering.
+    """,
+    packages=find_packages()
+)
